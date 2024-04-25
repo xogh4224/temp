f4923310a554f96f49b9f5a3910f8cfa32595cb4
djdjdddd <djdj2297@gmail.com>
2024-04-25 20:49:46 +0900

    Feat: 생성자, 수정자 필드 추가

--- src/main/java/com/gyungjo/manage/entity/BaseEntity.java
+++ src/main/java/com/gyungjo/manage/entity/BaseEntity.java
@@ -2,11 +2,12 @@ package com.gyungjo.manage.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -18,10 +19,22 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 생성일
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    // 수정일
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    // 생성자
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;
+
+    // 수정자
+    @LastModifiedBy
+    private String updatedBy;
+
 }
